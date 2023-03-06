735d8f1bb84bad824514dd163843fa993d2e7c4a
leechan1234 <124230969+leechan1234@users.noreply.github.com>
2023-03-06 12:26:28 +0900

    java 문법 문제풀이

---
+++ review_File01/array_And_Iteration.java
@@ -0,0 +1,17 @@
+package review_File01;
+
+import java.util.Scanner;
+
+public class array_And_Iteration {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String string = scanner.nextLine();
+        for (int a = 2; a <=2; a++) {
+            for (int b = 1; b <10; b++){
+                int result = a*b;
+                System.out.println(a +" * " + b + " = " + result);
+            }
+        }
+
+    }
+}
