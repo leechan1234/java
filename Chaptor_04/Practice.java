157c6e1e9da031513449ded74cf0db7e926c24ff
leechan1234 <124230969+leechan1234@users.noreply.github.com>
2023-03-07 13:09:36 +0900

    자바 기초

---
+++ Chaptor_04/Practice.java
@@ -0,0 +1,16 @@
+package Chaptor_04;
+
+import java.util.Scanner;
+
+public class Practice {
+    public static void main(String[] args) {
+        Scanner scanner =new Scanner(System.in);
+        String string = scanner.nextLine();
+
+        int a = 5;
+        int b = 4;
+         if (a >b){
+             System.out.println("<");
+         }
+    }
+}
--- _prob01/Main.java
+++ _prob01/Main.java
@@ -5,13 +5,19 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int score = scanner.nextInt();
 
-        System.out.println("|\\_/|");
-        System.out.println("|q p|   /}");
-        System.out.println("( 0 )\"\"\"\\");
-        System.out.println("|\"^\"`    |");
-        System.out.println("||_/=\\\\__|");
-
-}
-
+        if (score >= 90) {
+            System.out.println("A");
+        } else if (score >= 80) {
+            System.out.println("B");
+        } else if (score >= 70) {
+            System.out.println("C");
+        } else if (score >= 60) {
+            System.out.println("D");
+        } else {
+            System.out.println("F");
+        }
+    }
 }
