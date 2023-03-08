4a839c447d69f4867915e01634e267d7166dd217
leechan1234 <124230969+leechan1234@users.noreply.github.com>
2023-03-08 22:01:22 +0900

    자바 기초 복습

--- Chaptor_04/Practice.java
+++ Chaptor_04/Practice.java
@@ -4,13 +4,80 @@ import java.util.Scanner;
 
 public class Practice {
     public static void main(String[] args) {
-        Scanner scanner =new Scanner(System.in);
-        String string = scanner.nextLine();
 
-        int a = 5;
-        int b = 4;
-         if (a >b){
-             System.out.println("<");
+## &*숫자&*
+
+### 1. 숫자
+
+```java
+        int age = 10
+        long countOfStar = 8764827384923849L; //접미사로 L
+
+        자료형	표현범위
+        int	-2147483648 ~ 2147483647
+        long	-9223372036854775808 ~ 9223372036854775807
+```
+
+### 2. 실수
+
+```java
+        float pi = 3.14F; //접미사로 F
+        double morePi = 3.14159265358979323846;
+```
+
+### 3.숫자연산
+
+```java
+                + - * /
+        int a = 10;
+        int b = 5;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+```
+
+### 4.증감연산
+
+```java
+        int a = 1;
+        a++
+
+//2
+```
+
+## &*boolean&*
+
+```java
+        boolean isSuccess = true;
+
+        2 > 1             // 참
+        1 == 2            // 거짓
+        3 % 2 == 1        // 참 (3을 2로 나눈 나머지는 1이므로 참이다.)
+        "3".equals("2")   // 거짓
+
+//불리언의 조건문
+
+        int base = 180;
+        int height = 185;
+        boolean isTall = height > base;
+
+        if (isTall) {
+            System.out.println("키가 큽니다.");
+        }
+
+        키가 큽니다.
+
+        int i = 3;
+        boolean isOdd = i % 2 == 1;
+        System.out.println(isOdd);  // true 출력
+
+        true
+
+```
+
+## &문자&
          }
     }
 }
+        }
