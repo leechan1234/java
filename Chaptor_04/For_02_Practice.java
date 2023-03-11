f1bdfd99c0706b10b7c3f8b009d66550a0c3bcf1
leechan1234 <124230969+leechan1234@users.noreply.github.com>
2023-03-11 22:38:39 +0900

    자바 기초

---
+++ Chaptor_04/For_02_Practice.java
@@ -0,0 +1,21 @@
+package Chaptor_04;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class For_02_Practice {
+    public static void main(String[] args) {
+        String a = "write once, run anywhere";
+        if (a.contains("wife")) {
+            System.out.println("wife");
+        } else if (a.contains("once") && !a.contains("run")) {
+            System.out.println("once");
+        } else if (!a.contains("everywhere")) {
+            System.out.println("everywhere");
+        } else if (a.contains("anywhere")) {
+            System.out.println("anywhere");
+        } else {
+            System.out.println("none");
+        }
+    }
+}
---
+++ Chaptor_04/while_Problum.java
@@ -0,0 +1,18 @@
+package Chaptor_04;
+
+public class while_Problum {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 0;
+
+        if(a > b) {
+            c = a - b;
+        } else {
+            c = b - a;
+        }
+
+        System.out.println(c);
+    }
+
+}
